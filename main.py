27e174a7126c3e4b48d44a9e6818b435056dc8c9
JAYESK <83551716+Jayesk@users.noreply.github.com>
2022-04-14 23:00:05 +0000

    Checks if a streamer exists through filtering out inputs with special characters and spaces.

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+print("NUMBER ONE... WORKING...")
 import requests
 
-
 channelName = 'STREAMERNAME'
+print("NUMBER TWO... WORKING...")
+
+contents = requests.get('https://www.twitch.tv/' +channelName).content.decode('utf-8')
+
+special_characters = "\"!@#$%^&*()-+?_=,<>/\""
 
 if ' ' in channelName:
-  print("Input invalid.")
+	print("That channel does not exist!")
+
 else:
-
-  contents = requests.get('https://www.twitch.tv/' +channelName).content.decode('utf-8')
-
-  if 'isLiveBroadcast' in contents: 
-    print(channelName + ' is live')
-  else:
-    print(channelName + ' is not live')
+	
+	if any (c in special_characters for c in channelName):
+		print("That channel does not exist!")
+	
+	else:
+		if 'isLiveBroadcast' in contents:
+			print(channelName + ' is live')
+		else:
+			print(channelName + ' is not live')
